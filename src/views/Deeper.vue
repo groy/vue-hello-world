8581324d48792674ab58f99658287daaa34e8c0f
Greg Royan <greg@narrative.io>
2020-03-30 14:09:06 -0300

    Tweaks for demo

---
+++ src/views/Deeper.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div(class="about")
+    h2 Welcome to VueJs
+</template>
+
+<script>
+
+export default {
+  name: 'Deeper',
+  components: {},
+  props: {},
+  data(){
+    return {};
+  },
+  computed: {},
+  asyncComputed: {},
+  methods: {},
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+}
+</script>
+
+<style lang="sass">
+.about
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center 
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="inputs">
+      <label>First Name:</label>
+      <input v-model="firstName" />
+      <label>Last Name:</label>
+      <input v-model="lastName" />
+    </div>
+    <h1>Hello<span v-if="firstName || lastName">, {{ fullName }}</span>!</h1>
+    <h2>Welcome to VueJs</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data(){
+    return {
+      firstName: null,
+      lastName: null,
+    };
+  },
+  computed: {
+    fullName() {
+      if (this.firstName && !this.lastName) {
+        return this.firstName;
+      } else if (this.lastName && !this.firstName) {
+        return this.lastName;
+      }
+      return `${this.firstName} ${this.lastName}`
+    }
   }
 }
 </script>
+
+<style lang="sass">
+.home
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  .inputs
+    display: flex
+    flex-direction: column
+    justify-content: flex-start
+    align-items: flex-start
+    input
+      margin: 0 0 12px 0  
+</style>
